d81e76bb44f078493aa3fe0143d9fa4c7065c5dc
fvn70 <fvn_reg@mail.ru>
2022-02-12 10:51:08 +0700

    new Stage 3/5

--- Markdown Editor/task/editor.py
+++ Markdown Editor/task/editor.py
@@ -1,12 +1,53 @@
+def plain():
+    return input("Text: ")
 
-cmd_set = set("plain bold italic header link inline-code ordered-list unordered-list new-line !help !done".split())
+
+def bold():
+    return "**" + input("Text: ") + "**"
+
+
+def italic():
+    return "*" + input("Text: ") + "*"
+
+
+def header():
+    while True:
+        level = int(input("Level: "))
+        if level in range(1, 7):
+            break
+        else:
+            print("The level should be within the range of 1 to 6")
+    txt = input("Text: ")
+    return "#" * level + " " + txt + "\n"
+
+
+def link():
+    label = input("Label: ")
+    url = input("URL: ")
+    return "[" + label + "](" + url + ")"
+
+
+def inline_code():
+    txt = input("Text: ")
+    return "`" + txt + "`"
+
+
+def new_line():
+    return "\n"
+
+
+cmd_set = set("plain bold italic header link inline-code new-line !help !done".split())
 text = ""
 while True:
     cmd = input("Choose a formatter: ")
     if cmd not in cmd_set:
         print("Unknown formatting type or command")
     elif cmd == "!help":
-        print("Available formatters: plain bold italic header link inline-code ordered-list unordered-list new-line")
+        print("Available formatters: plain bold italic header link inline-code new-line")
         print("Special commands: !help !done")
     elif cmd == "!done":
         break
+    else:
+        cmd = cmd.replace("-", "_")
+        text += eval(cmd + "()")
+        print(text)
