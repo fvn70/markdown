5033fba035cbe5a2bd2fe4ef60803cb8d157fdad
fvn70 <fvn_reg@mail.ru>
2022-02-12 10:20:44 +0700

    new Stage 2/5

--- Markdown Editor/task/editor.py
+++ Markdown Editor/task/editor.py
@@ -1,9 +1,12 @@
-# write your code here
-print("""# John Lennon
-or ***John Winston Ono Lennon*** was one of *The Beatles*.
-Here are the songs he wrote I like the most:
-* Imagine
-* Norwegian Wood
-* Come Together
-* In My Life
-* ~~Hey Jude~~ (that was *McCartney*)""")
+
+cmd_set = set("plain bold italic header link inline-code ordered-list unordered-list new-line !help !done".split())
+text = ""
+while True:
+    cmd = input("Choose a formatter: ")
+    if cmd not in cmd_set:
+        print("Unknown formatting type or command")
+    elif cmd == "!help":
+        print("Available formatters: plain bold italic header link inline-code ordered-list unordered-list new-line")
+        print("Special commands: !help !done")
+    elif cmd == "!done":
+        break
